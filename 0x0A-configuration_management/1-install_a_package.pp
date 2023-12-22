46e4920a654ce9f12804bd70ab04c01fcc305904
benaichalae <benaichalae@gmail.com>
2023-12-22 12:18:11 -0800

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,6 @@
-# Installs flask from pip3
+# install flask from pip3
 
-package { 'python3-pip':
-  ensure => present,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => '/usr/local/bin:/usr/bin:/bin',
-  refreshonly => true,
-}
-
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Exec['install_flask'],
 }
-
