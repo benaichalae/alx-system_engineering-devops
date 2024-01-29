e2baae95bdeab1a1344ee60d3d31645797d10c3a
benaichalae <benaichalae@gmail.com>
2024-01-29 13:38:18 -0800

    py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
-""" Contains a python script that, using the JSONplaceholder"""
+""" Contains a Python script that,
+using the JSONPlaceholder API,
+exports TODO list data to a CSV file."""
 import csv
 import requests
 from sys import argv
 
 
-def export_to_csv(employee_id, user_data, todo_data):
-    filename = "{}.csv".format(employee_id)
-    with open(filename, mode='w', newline='') as csv_file:
+def export_to_csv(user_id, username, tasks):
+    filename = "{}.csv".format(user_id)
+    with open(filename, 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         csv_writer.writerow(
                 ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
                 )
-
-        for task in todo_data:
+        for task in tasks:
             csv_writer.writerow(
-                    [employee_id, user_data['id'], user_data['username'],
-                        str(task['completed']), task['title']]
+                    [user_id, username, task['completed'], task['title']]
                     )
-
     print("CSV file '{}' has been created.".format(filename))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(argv) != 2 or not argv[1].isdigit():
-        print("Usage: {} <employee_id>".format(argv[0]))
+        print("Usage: {} <user_id>".format(argv[0]))
     else:
-        employee_id = argv[1]
+        user_id = argv[1]
 
         user_response = requests.get(
                 'https://jsonplaceholder.typicode.com/users/{}'.format(
-                    employee_id
+                    user_id
                     )
                 )
         user_data = user_response.json()
@@ -40,9 +39,9 @@ if __name__ == "__main__":
         else:
             todo_response = requests.get(
                 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-                    employee_id
+                    user_id
                     )
                 )
             todo_data = todo_response.json()
 
-            export_to_csv(employee_id, user_data, todo_data)
+            export_to_csv(user_id, user_data['username'], todo_data)
