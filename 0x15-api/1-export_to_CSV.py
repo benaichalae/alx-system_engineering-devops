ecda3ca991dd8ac716143e8b739c29c6d9677bcf
benaichalae <benaichalae@gmail.com>
2024-01-29 13:26:40 -0800

    py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Contains a python script that, using the JSONplaceholder"""
+import requests
+import csv
+from sys import argv
+
+
+def export_to_csv(employee_id, user_data, todo_data):
+    filename = "{}.csv".format(employee_id)
+    with open(filename, mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        csv_writer.writerow(
+                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+                )
+
+        for task in todo_data:
+            csv_writer.writerow(
+                    [employee_id, user_data['username'],
+                        str(task['completed']), task['title']]
+                    )
+
+    print("CSV file '{}' has been created.".format(filename))
+
+
+if __name__ == "__main__":
+    if len(argv) != 2 or not argv[1].isdigit():
+        print("Usage: {} <employee_id>".format(argv[0]))
+    else:
+        employee_id = argv[1]
+
+        user_response = requests.get(
+                'https://jsonplaceholder.typicode.com/users/{}'.format(
+                    employee_id
+                    )
+                )
+        user_data = user_response.json()
+
+        todo_response = requests.get(
+                'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+                    employee_id
+                    )
+                )
+        todo_data = todo_response.json()
+
+        export_to_csv(employee_id, user_data, todo_data)
