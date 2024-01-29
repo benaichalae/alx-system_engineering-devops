ad1963cc73e70149ad55704187935fbaf12ca9bb
benaichalae <benaichalae@gmail.com>
2024-01-29 13:51:59 -0800

    py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,14 +4,20 @@ import csv
 import requests
 from sys import argv
 
+
 def export_to_csv(user_id, username, tasks):
     filename = "{}.csv".format(user_id)
     with open(filename, 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        csv_writer.writerow(
+                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+                )
         for task in tasks:
-            csv_writer.writerow([user_id, username, str(task['completed']), task['title']])
+            csv_writer.writerow(
+                    [user_id, username, str(task['completed']), task['title']]
+                    )
     print("CSV file '{}' has been created.".format(filename))
+
 
 if __name__ == "__main__":
     if len(argv) != 2 or not argv[1].isdigit():
@@ -19,13 +25,21 @@ if __name__ == "__main__":
     else:
         employee_id = int(argv[1])
 
-        user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+        user_response = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(
+                employee_id
+                )
+            )
         user_data = user_response.json()
 
         if 'id' not in user_data or 'username' not in user_data:
             print("Invalid user ID.")
         else:
-            todo_response = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id))
+            todo_response = requests.get(
+                'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+                    employee_id
+                    )
+                )
             todo_data = todo_response.json()
 
             export_to_csv(employee_id, user_data['username'], todo_data)
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,12 +4,18 @@ import json
 import requests
 from sys import argv
 
+
 def export_to_json(user_id, username, tasks):
-    data = {user_id: [{"task": task['title'], "completed": task['completed'], "username": username} for task in tasks]}
+    data = {user_id: [{
+        "task": task['title'],
+        "completed": task['completed'],
+        "username": username
+        } for task in tasks]}
     filename = "{}.json".format(user_id)
     with open(filename, 'w') as json_file:
         json.dump(data, json_file)
     print("JSON file '{}' has been created.".format(filename))
+
 
 if __name__ == "__main__":
     if len(argv) != 2 or not argv[1].isdigit():
@@ -17,13 +23,21 @@ if __name__ == "__main__":
     else:
         employee_id = int(argv[1])
 
-        user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+        user_response = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(
+                employee_id
+                )
+            )
         user_data = user_response.json()
 
         if 'id' not in user_data or 'username' not in user_data:
             print("Invalid user ID.")
         else:
-            todo_response = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id))
+            todo_response = requests.get(
+                'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+                    employee_id
+                    )
+                )
             todo_data = todo_response.json()
 
             export_to_json(employee_id, user_data['username'], todo_data)
