162293ed40b8ae53216932bca9bef387b6947b1a
benaichalae <benaichalae@gmail.com>
2024-02-14 09:01:23 -0800

    pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,10 @@
 # 0-strace_is_your_friend.pp
 
-# Define an exec resource to fix the issue causing the Apache 500 error
+$file_to_edit = '/var/www/html/wp-settings.php'
 
-exec { 'fix_apache_issue':
-  command     => 'your_fix_command_here',
-  refreshonly => true,
-  path        => '/bin:/usr/bin', # Adjust the path as necessary
-}
-
-# Trigger the fix only when Apache returns a 500 error
-
-notify { 'fix_apache_issue':
-  subscribe => Service['apache2'],
-  refreshonly => true,
+# Define an exec resource to replace the line in the file
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => "grep -q 'phpp' ${file_to_edit}", # Only execute if the line exists
 }
